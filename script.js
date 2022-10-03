2e04824b4437d2382817cf26f00e522d2fdb7d19
Okoro James <73547857+jamextech1@users.noreply.github.com>
2022-10-03 21:23:32 +0100

    Updated My Script.js Add To Cart Button
    JavaScript Add To Cart Button Feature Now Working Perfectly!!

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const btn = document.querySelector(".btn--small");
-
-btn.addEventListener("click", function () {
-  setTimeout(function () {
-    btn.style.backgroundColor = "#166534";
-    btn.style.color = "#fafafa";
-    btn.style.border = "none";
-    btn.innerHTML = "Added To Cart";
-  }, 10000);
+const btn = document.querySelectorAll(".btn--small");
+btn.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault;
+    setTimeout(function () {
+      btn.style.backgroundColor = "#166534";
+      btn.style.color = "#fafafa";
+      btn.style.border = "none";
+      btn.textContent = "Added To Cart";
+    }, 5000);
+  });
 });
