35cba3198ec8ff94f3e2271655c68825db987b95
Okoro James <73547857+jamextech1@users.noreply.github.com>
2022-10-05 22:11:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btn = document.querySelectorAll(".btn--small");
+const img_display_none = document.querySelectorAll(".click_image");
 btn.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     e.preventDefault;
@@ -7,6 +8,10 @@ btn.forEach(function (btn) {
       btn.style.color = "#fafafa";
       btn.style.border = "none";
       btn.textContent = "Added To Cart";
+      img_display_none.forEach(function (img_display_none) {
+        img_display_none.style.display = "none";
+        alert("Added To Cart!");
+      });
     }, 10000);
   });
 });
